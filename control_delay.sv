d979eb7875c9d224077987e037055204e5724ef9
willrobbins24 <william.r.robbins@gmail.com>
2026-09-12 16:15:36 -0400

    Update control_delay.sv

--- control_delay.sv
+++ control_delay.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/26/2026 02:48:04 PM
-// Design Name: 
-// Module Name: control_delay
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module control_delay#(
     parameter int WIDTH = 32,
